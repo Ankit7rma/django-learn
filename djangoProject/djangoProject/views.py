c46a9acfdd5c3365b375303d68af1ccc0bb6f61f
Ankit7rma <ankitsharma290101@gmail.com>
2024-09-04 18:41:51 +0530

    templates

--- djangoProject/djangoProject/views.py
+++ djangoProject/djangoProject/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 def home(request):
-    return HttpResponse("Hello World , This is Home Page.")
+    # return HttpResponse("Hello World , This is Home Page.")
+    return render(request,"index.html")
 def about(request):
     return HttpResponse("Hello World , This is About Page.")
 def contact(request):
