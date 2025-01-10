bb5cb9fa93411d913439d876e054cef2a6b34994
Ankit Sharma <ankitsharma290101@gmail.com>
2025-01-10 16:15:01 +0530

    path

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -20,5 +20,5 @@ from django.urls import path , include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("newapp.urls") )
+    path("main/", include("newapp.urls") )
 ]
--- myproject/newapp/views.py
+++ myproject/newapp/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("Welcome to Home Page")
+    path = request.path
+    return HttpResponse(path,content_type="text/html", charset="utf-8")
