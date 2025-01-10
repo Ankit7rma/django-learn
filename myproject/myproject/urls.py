e5761541ffceb9c790c92e6707a22621a7068c90
Ankit Sharma <ankitsharma290101@gmail.com>
2025-01-10 16:16:31 +0530

    a

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path , include
-
+# a
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("main/", include("newapp.urls") )
